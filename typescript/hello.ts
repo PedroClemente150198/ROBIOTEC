ee5f7472a7ca01aeae24854fc3bc41abcb142784
PedroClemente <pedro_clemente48@hotmail.com>
2026-06-24 18:15:28 -0500

    Feat: Sección o Page de contacto

--- typescript/hello.ts
+++ typescript/hello.ts
@@ -13,4 +13,4 @@ function area(shape: Shape){
 
 console.log(area({name: "rectangle", width: 30, height: 15}));
 console.log(area({name: "square", width: 20, height: 20, color: "red"}));
-console.log( area({width: 30, height: 15}));
+console.log( area({width: 30, height: 15}));2024
